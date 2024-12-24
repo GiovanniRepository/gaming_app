12cf595f7c0b5c058af52c8e079fb3498375ca3d
GiovanniRepository <giovanni.imasso@edu.itspiemonte.it>
2024-12-24 16:59:42 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'homeview.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homeview(),
+    );
+  }
+}
+
+
+//Todo: rendere tutta l'app responsive automaticamente
+//Todo: creare un tema personalizzato
+//Todo: gestire le cartelle e i file
+//Todo: creare un file di costanti per i colori
+//Todo: aggiungere assets
